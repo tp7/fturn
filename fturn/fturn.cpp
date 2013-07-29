097ac9491980298b06fc26b4c4ff70f388861063
tp7 <tp7ptr@gmail.com>
2013-07-29 16:48:58 +1100

    Make FTurnLeft SSE2-compatible

--- fturn/fturn.cpp
+++ fturn/fturn.cpp
@@ -19,6 +19,11 @@ enum class TurnDirection {
     TURN180
 };
 
+enum class InstructionSet {
+    SSE2,
+    SSSE3
+};
+
 bool isSupportedColorspace(int pixelType) {
     if (pixelType == VideoInfo::CS_YV12 || pixelType == VideoInfo::CS_I420) {
         return true;
@@ -45,8 +50,8 @@ bool hasChroma(int pixelType) {
     return true;
 }
 
-__forceinline void fturn(__m128i &src1, __m128i &src2, __m128i& src3, __m128i &src4, 
-                             __m128i &src5, __m128i& src6, __m128i &src7, __m128i &src8, const __m128i &mask) {
+__forceinline void fTranspose(__m128i &src1, __m128i &src2, __m128i& src3, __m128i &src4, 
+                             __m128i &src5, __m128i& src6, __m128i &src7, __m128i &src8) {
     auto a = _mm_unpacklo_epi8((src1), (src1)); 
     auto b = _mm_unpacklo_epi8((src2), (src2)); 
     auto c = _mm_unpacklo_epi8((src3), (src3)); 
@@ -74,25 +79,15 @@ __forceinline void fturn(__m128i &src1, __m128i &src2, __m128i& src3, __m128i &s
     auto e45f45g45h45 = _mm_unpacklo_epi32(e47f47, g47h47); 
     auto e67f67g67h67 = _mm_unpackhi_epi32(e47f47, g47h47); 
 
-    auto a0b0c0d0e0f0g0h0 = _mm_unpacklo_epi64(a01b01c01d01, e01f01g01h01); 
-    auto a1b1c1d1e1f1g1h1 = _mm_unpackhi_epi64(a01b01c01d01, e01f01g01h01); 
-    auto a2b2c2d2e2f2g2h2 = _mm_unpacklo_epi64(a23b23c23d23, e23f23g23h23); 
-    auto a3b3c3d3e3f3g3h3 = _mm_unpackhi_epi64(a23b23c23d23, e23f23g23h23); 
-    auto a4b4c4d4e4f4g4h4 = _mm_unpacklo_epi64(a45b45c45d45, e45f45g45h45); 
-    auto a5b5c5d5e5f5g5h5 = _mm_unpackhi_epi64(a45b45c45d45, e45f45g45h45); 
-    auto a6b6c6d6e6f6g6h6 = _mm_unpacklo_epi64(a67b67c67d67, e67f67g67h67); 
-    auto a7b7c7d7e7f7g7h7 = _mm_unpackhi_epi64(a67b67c67d67, e67f67g67h67); 
-
-    src1 = _mm_shuffle_epi8(a0b0c0d0e0f0g0h0, mask); 
-    src2 = _mm_shuffle_epi8(a1b1c1d1e1f1g1h1, mask); 
-    src3 = _mm_shuffle_epi8(a2b2c2d2e2f2g2h2, mask); 
-    src4 = _mm_shuffle_epi8(a3b3c3d3e3f3g3h3, mask); 
-    src5 = _mm_shuffle_epi8(a4b4c4d4e4f4g4h4, mask); 
-    src6 = _mm_shuffle_epi8(a5b5c5d5e5f5g5h5, mask); 
-    src7 = _mm_shuffle_epi8(a6b6c6d6e6f6g6h6, mask); 
-    src8 = _mm_shuffle_epi8(a7b7c7d7e7f7g7h7, mask);
+    src1 = _mm_unpacklo_epi64(a01b01c01d01, e01f01g01h01); 
+    src2 = _mm_unpackhi_epi64(a01b01c01d01, e01f01g01h01); 
+    src3 = _mm_unpacklo_epi64(a23b23c23d23, e23f23g23h23); 
+    src4 = _mm_unpackhi_epi64(a23b23c23d23, e23f23g23h23); 
+    src5 = _mm_unpacklo_epi64(a45b45c45d45, e45f45g45h45); 
+    src6 = _mm_unpackhi_epi64(a45b45c45d45, e45f45g45h45); 
+    src7 = _mm_unpacklo_epi64(a67b67c67d67, e67f67g67h67); 
+    src8 = _mm_unpackhi_epi64(a67b67c67d67, e67f67g67h67); 
 }
-
 
 
 void turnPlaneRight(BYTE* pDst, const BYTE* pSrc, BYTE* buffer, int srcWidth, int srcHeight, int dstPitch, int srcPitch) {
@@ -121,7 +116,16 @@ void turnPlaneRight(BYTE* pDst, const BYTE* pSrc, BYTE* buffer, int srcWidth, in
             auto src7 = _mm_loadl_epi64(reinterpret_cast<const __m128i*>(pSrc+x+srcPitch*6));
             auto src8 = _mm_loadl_epi64(reinterpret_cast<const __m128i*>(pSrc+x+srcPitch*7));
 
-            fturn(src1, src2, src3, src4, src5, src6, src7, src8, mask);
+            fTranspose(src1, src2, src3, src4, src5, src6, src7, src8);
+
+            src1 = _mm_shuffle_epi8(src1, mask); 
+            src2 = _mm_shuffle_epi8(src2, mask); 
+            src3 = _mm_shuffle_epi8(src3, mask); 
+            src4 = _mm_shuffle_epi8(src4, mask); 
+            src5 = _mm_shuffle_epi8(src5, mask); 
+            src6 = _mm_shuffle_epi8(src6, mask); 
+            src7 = _mm_shuffle_epi8(src7, mask); 
+            src8 = _mm_shuffle_epi8(src8, mask);
 
             _mm_storel_epi64(reinterpret_cast<__m128i*>(buffer+offset+srcHeight*0), src1);
             _mm_storel_epi64(reinterpret_cast<__m128i*>(buffer+offset+srcHeight*1), src2);
@@ -174,6 +178,7 @@ void turnPlaneRight(BYTE* pDst, const BYTE* pSrc, BYTE* buffer, int srcWidth, in
     }
 }
 
+template<InstructionSet level>
 void turnPlaneLeft(BYTE* pDst, const BYTE* pSrc, BYTE* buffer, int srcWidth, int srcHeight, int dstPitch, int srcPitch) {
     bool useBuffer = true;
     if (dstPitch == srcHeight) {
@@ -187,7 +192,15 @@ void turnPlaneLeft(BYTE* pDst, const BYTE* pSrc, BYTE* buffer, int srcWidth, int
     int srcHeightMod8 = (srcHeight / 8) * 8;
 
     pSrc += srcWidth-8;
-    auto mask = _mm_set_epi8(0, 0, 0, 0, 0, 0, 0, 0, 14, 12, 10, 8, 6, 4, 2, 0);
+    __m128i pandMask, pshufbMask, zero;
+
+    if (level == InstructionSet::SSE2) {
+        pandMask = _mm_set_epi8(0, 0xFF, 0, 0xFF, 0, 0xFF, 0, 0xFF, 0, 0xFF, 0, 0xFF, 0, 0xFF, 0, 0xFF);
+        zero = _mm_setzero_si128();
+    }
+    else {
+        pshufbMask = _mm_set_epi8(0, 0, 0, 0, 0, 0, 0, 0, 14, 12, 10, 8, 6, 4, 2, 0);
+    }
     for(int y=0; y<srcHeightMod8; y+=8)
     {
         int offset = y;
@@ -202,7 +215,36 @@ void turnPlaneLeft(BYTE* pDst, const BYTE* pSrc, BYTE* buffer, int srcWidth, int
             auto src7 = _mm_loadl_epi64(reinterpret_cast<const __m128i*>(pSrc-x+srcPitch*6));
             auto src8 = _mm_loadl_epi64(reinterpret_cast<const __m128i*>(pSrc-x+srcPitch*7));
 
-            fturn(src1, src2, src3, src4, src5, src6, src7, src8, mask);
+            fTranspose(src1, src2, src3, src4, src5, src6, src7, src8);
+
+            if (level == InstructionSet::SSE2) {
+                src1 =  _mm_and_si128(src1, pandMask);
+                src2 =  _mm_and_si128(src2, pandMask);
+                src3 =  _mm_and_si128(src3, pandMask);
+                src4 =  _mm_and_si128(src4, pandMask);
+                src5 =  _mm_and_si128(src5, pandMask);
+                src6 =  _mm_and_si128(src6, pandMask);
+                src7 =  _mm_and_si128(src7, pandMask);
+                src8 =  _mm_and_si128(src8, pandMask);
+
+                src1 =  _mm_packus_epi16(src1, zero); 
+                src2 =  _mm_packus_epi16(src2, zero); 
+                src3 =  _mm_packus_epi16(src3, zero); 
+                src4 =  _mm_packus_epi16(src4, zero); 
+                src5 =  _mm_packus_epi16(src5, zero); 
+                src6 =  _mm_packus_epi16(src6, zero); 
+                src7 =  _mm_packus_epi16(src7, zero); 
+                src8 =  _mm_packus_epi16(src8, zero);
+            } else {
+                src1 = _mm_shuffle_epi8(src1, pshufbMask); 
+                src2 = _mm_shuffle_epi8(src2, pshufbMask); 
+                src3 = _mm_shuffle_epi8(src3, pshufbMask); 
+                src4 = _mm_shuffle_epi8(src4, pshufbMask); 
+                src5 = _mm_shuffle_epi8(src5, pshufbMask); 
+                src6 = _mm_shuffle_epi8(src6, pshufbMask); 
+                src7 = _mm_shuffle_epi8(src7, pshufbMask); 
+                src8 = _mm_shuffle_epi8(src8, pshufbMask);
+            }
 
             _mm_storel_epi64(reinterpret_cast<__m128i*>(buffer+offset+srcHeight*0), src8);
             _mm_storel_epi64(reinterpret_cast<__m128i*>(buffer+offset+srcHeight*1), src7);
@@ -304,6 +346,9 @@ void turnPlane180C(BYTE* pDst, const BYTE* pSrc, BYTE*, int srcWidth, int srcHei
     }
 }
 
+auto turnPlaneLeftSSE2 = &turnPlaneLeft<InstructionSet::SSE2>;
+auto turnPlaneLeftSSSE3 = &turnPlaneLeft<InstructionSet::SSSE3>;
+
 class FTurn : public GenericVideoFilter {
 public:
     FTurn(PClip child, TurnDirection direction, bool chroma, bool mt, IScriptEnvironment* env);
@@ -316,7 +361,7 @@ public:
 private:
     bool chroma_;
     bool mt_;
-    decltype(&turnPlaneLeft) turnFunction_;
+    decltype(&turnPlane180SSE3) turnFunction_;
     BYTE *buffer;
     BYTE *bufferUV;
 };
@@ -340,7 +385,11 @@ FTurn::FTurn(PClip child, TurnDirection direction, bool chroma, bool mt, IScript
         vi.width = child->GetVideoInfo().height;
         vi.height = child->GetVideoInfo().width;
 
-        turnFunction_ = direction == TurnDirection::RIGHT ? turnPlaneRight : turnPlaneLeft;
+        if (direction == TurnDirection::LEFT) {
+            turnFunction_ = sse3 ? turnPlaneLeftSSSE3 : turnPlaneLeftSSE2;
+        } else {
+            turnFunction_ = turnPlaneRight;
+        }
 
         buffer = new BYTE[vi.width*vi.height];
 
