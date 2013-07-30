68701586779e27e2f84f4a4ab4b66105f53f35b7
tp7 <tp7ptr@gmail.com>
2013-07-30 15:12:01 +1100

    Simplify SSE2 SIMD a bit.
    Also marginally faster

--- fturn/fturn.cpp
+++ fturn/fturn.cpp
@@ -51,15 +51,15 @@ bool hasChroma(int pixelType) {
 }
 
 __forceinline void fTranspose(__m128i &src1, __m128i &src2, __m128i& src3, __m128i &src4, 
-                             __m128i &src5, __m128i& src6, __m128i &src7, __m128i &src8) {
-    auto a = _mm_unpacklo_epi8((src1), (src1)); 
-    auto b = _mm_unpacklo_epi8((src2), (src2)); 
-    auto c = _mm_unpacklo_epi8((src3), (src3)); 
-    auto d = _mm_unpacklo_epi8((src4), (src4)); 
-    auto e = _mm_unpacklo_epi8((src5), (src5)); 
-    auto f = _mm_unpacklo_epi8((src6), (src6)); 
-    auto g = _mm_unpacklo_epi8((src7), (src7)); 
-    auto h = _mm_unpacklo_epi8((src8), (src8)); 
+                             __m128i &src5, __m128i& src6, __m128i &src7, __m128i &src8, const __m128i &zero) {
+    auto a = _mm_unpacklo_epi8(src1, zero); 
+    auto b = _mm_unpacklo_epi8(src2, zero); 
+    auto c = _mm_unpacklo_epi8(src3, zero); 
+    auto d = _mm_unpacklo_epi8(src4, zero); 
+    auto e = _mm_unpacklo_epi8(src5, zero); 
+    auto f = _mm_unpacklo_epi8(src6, zero); 
+    auto g = _mm_unpacklo_epi8(src7, zero); 
+    auto h = _mm_unpacklo_epi8(src8, zero); 
 
     auto a03b03 = _mm_unpacklo_epi16(a, b); 
     auto c03d03 = _mm_unpacklo_epi16(c, d);
@@ -89,11 +89,10 @@ __forceinline void fTranspose(__m128i &src1, __m128i &src2, __m128i& src3, __m12
     src8 = _mm_unpackhi_epi64(a67b67c67d67, e67f67g67h67); 
 }
 
-__forceinline __m128i halfRotateEpi16(__m128i src, const __m128i& pandMask) {
+__forceinline __m128i rotateEpi16(__m128i src) {
     auto result = _mm_shuffle_epi32(src, _MM_SHUFFLE(1, 0, 3, 2));
     result = _mm_shufflelo_epi16(result, _MM_SHUFFLE(0, 1, 2, 3));
-    result = _mm_shufflehi_epi16(result, _MM_SHUFFLE(0, 1, 2, 3));
-    return _mm_and_si128(result, pandMask);
+    return _mm_shufflehi_epi16(result, _MM_SHUFFLE(0, 1, 2, 3));
 }
 
 
@@ -108,14 +107,11 @@ void turnPlaneRight(BYTE* pDst, const BYTE* pSrc, BYTE* buffer, int srcWidth, in
     auto pDst2 = pDst;
     auto pSrc2 = pSrc;
 
-    __m128i pandMask, zero, pshufbMask;
+    __m128i zero, pshufbMask;
 
-    if (level == InstructionSet::SSE2) {
-        #pragma warning(disable: 4309)
-        pandMask = _mm_set_epi8(0, 0xFF, 0, 0xFF, 0, 0xFF, 0, 0xFF, 0, 0xFF, 0, 0xFF, 0, 0xFF, 0, 0xFF);
-        #pragma warning(default: 4309)
-        zero = _mm_setzero_si128();
-    } else {
+    zero = _mm_setzero_si128();
+
+    if (level == InstructionSet::SSSE3) {
         pshufbMask = _mm_set_epi8(0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 4, 6, 8, 10, 12, 14);
     }
 
@@ -135,17 +131,17 @@ void turnPlaneRight(BYTE* pDst, const BYTE* pSrc, BYTE* buffer, int srcWidth, in
             auto src7 = _mm_loadl_epi64(reinterpret_cast<const __m128i*>(pSrc+x+srcPitch*6));
             auto src8 = _mm_loadl_epi64(reinterpret_cast<const __m128i*>(pSrc+x+srcPitch*7));
 
-            fTranspose(src1, src2, src3, src4, src5, src6, src7, src8);
+            fTranspose(src1, src2, src3, src4, src5, src6, src7, src8, zero);
             
             if (level == InstructionSet::SSE2) {
-                src1 = halfRotateEpi16(src1, pandMask);
-                src2 = halfRotateEpi16(src2, pandMask);
-                src3 = halfRotateEpi16(src3, pandMask);
-                src4 = halfRotateEpi16(src4, pandMask);
-                src5 = halfRotateEpi16(src5, pandMask);
-                src6 = halfRotateEpi16(src6, pandMask);
-                src7 = halfRotateEpi16(src7, pandMask);
-                src8 = halfRotateEpi16(src8, pandMask);
+                src1 = rotateEpi16(src1);
+                src2 = rotateEpi16(src2);
+                src3 = rotateEpi16(src3);
+                src4 = rotateEpi16(src4);
+                src5 = rotateEpi16(src5);
+                src6 = rotateEpi16(src6);
+                src7 = rotateEpi16(src7);
+                src8 = rotateEpi16(src8);
 
                 src1 = _mm_packus_epi16(src1, zero);
                 src2 = _mm_packus_epi16(src2, zero);
@@ -231,17 +227,14 @@ void turnPlaneLeft(BYTE* pDst, const BYTE* pSrc, BYTE* buffer, int srcWidth, int
     int srcHeightMod8 = (srcHeight / 8) * 8;
 
     pSrc += srcWidth-8;
-    __m128i pandMask, pshufbMask, zero;
+    __m128i pshufbMask, zero;
 
-    if (level == InstructionSet::SSE2) {
-        #pragma warning(disable: 4309)
-        pandMask = _mm_set_epi8(0, 0xFF, 0, 0xFF, 0, 0xFF, 0, 0xFF, 0, 0xFF, 0, 0xFF, 0, 0xFF, 0, 0xFF);
-        #pragma warning(default: 4309)
-        zero = _mm_setzero_si128();
-    }
-    else {
+    zero = _mm_setzero_si128();
+
+    if (level == InstructionSet::SSSE3) {
         pshufbMask = _mm_set_epi8(0, 0, 0, 0, 0, 0, 0, 0, 14, 12, 10, 8, 6, 4, 2, 0);
     }
+
     for(int y=0; y<srcHeightMod8; y+=8)
     {
         int offset = y;
@@ -256,18 +249,9 @@ void turnPlaneLeft(BYTE* pDst, const BYTE* pSrc, BYTE* buffer, int srcWidth, int
             auto src7 = _mm_loadl_epi64(reinterpret_cast<const __m128i*>(pSrc-x+srcPitch*6));
             auto src8 = _mm_loadl_epi64(reinterpret_cast<const __m128i*>(pSrc-x+srcPitch*7));
 
-            fTranspose(src1, src2, src3, src4, src5, src6, src7, src8);
+            fTranspose(src1, src2, src3, src4, src5, src6, src7, src8, zero);
 
             if (level == InstructionSet::SSE2) {
-                src1 =  _mm_and_si128(src1, pandMask);
-                src2 =  _mm_and_si128(src2, pandMask);
-                src3 =  _mm_and_si128(src3, pandMask);
-                src4 =  _mm_and_si128(src4, pandMask);
-                src5 =  _mm_and_si128(src5, pandMask);
-                src6 =  _mm_and_si128(src6, pandMask);
-                src7 =  _mm_and_si128(src7, pandMask);
-                src8 =  _mm_and_si128(src8, pandMask);
-
                 src1 =  _mm_packus_epi16(src1, zero); 
                 src2 =  _mm_packus_epi16(src2, zero); 
                 src3 =  _mm_packus_epi16(src3, zero); 
@@ -348,12 +332,9 @@ void turnPlane180(BYTE* pDst, const BYTE* pSrc, BYTE*, int srcWidth, int srcHeig
     const BYTE* pSrc2 = pSrc;
     int srcWidthMod16 = (srcWidth / 16) * 16;
 
-    __m128i pandMask, zero, pshufbMask;
+    __m128i zero, pshufbMask;
 
     if (level == InstructionSet::SSE2) {
-        #pragma warning(disable: 4309)
-        pandMask = _mm_set_epi8(0, 0xFF, 0, 0xFF, 0, 0xFF, 0, 0xFF, 0, 0xFF, 0, 0xFF, 0, 0xFF, 0, 0xFF);
-        #pragma warning(default: 4309)
         zero = _mm_setzero_si128();
     } else {
         pshufbMask = _mm_set_epi8(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15);
@@ -369,8 +350,8 @@ void turnPlane180(BYTE* pDst, const BYTE* pSrc, BYTE*, int srcWidth, int srcHeig
             if (level == InstructionSet::SSE2) {
                 auto low = _mm_unpacklo_epi8(src, zero);
                 auto high = _mm_unpackhi_epi8(src, zero);
-                low = halfRotateEpi16(low, pandMask);
-                high = halfRotateEpi16(high, pandMask);
+                low = rotateEpi16(low);
+                high = rotateEpi16(high);
                 src = _mm_packus_epi16(high, low);
             } else { 
                 src = _mm_shuffle_epi8(src, pshufbMask);
@@ -428,12 +409,12 @@ FTurn::FTurn(PClip child, TurnDirection direction, bool chroma, bool mt, IScript
     if (!(env->GetCPUFlags() & CPUF_SSE2)) {
         env->ThrowError("Sorry, at least SSE2 is required");
     }
-
+    
     int CPUInfo[4]; //eax, ebx, ecx, edx
     __cpuid(CPUInfo, 1);
 
     #pragma warning(disable: 4800)
-    bool ssse3 = CPUInfo[2] & 0x00000200;
+    bool ssse3 = false;//CPUInfo[2] & 0x00000200;
     #pragma warning(disable: 4800)
 
     if (direction == TurnDirection::RIGHT || direction == TurnDirection::LEFT) {
